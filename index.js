746f337e2ed11d4712287b157e4223e96ca69957
Laz <nyys811@aol.com>
2023-06-25 20:53:45 -0400

    created the function to write the Readme using fs

--- index.js
+++ index.js
@@ -55,7 +55,15 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('README.md file generated successfully!');
+        }
+      });
+}
 
 // TODO: Create a function to initialize app
 function init() {}

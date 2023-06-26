3c646b4586258dd3a36ce592eca7037400d7ff53
Laz <nyys811@aol.com>
2023-06-25 21:02:05 -0400

    filled out the init function using inquirer setup and markdown

--- index.js
+++ index.js
@@ -66,7 +66,12 @@ function writeToFile(fileName, data) {
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const markdown = generateMarkdown(answers);
+        writeToFile('README.md', markdown);
+      });
+}
 
 // Function call to initialize app
 init();

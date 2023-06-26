459d3583c3bce1a52341d1081f2489ef1b6ce1bc
Laz <nyys811@aol.com>
2023-06-25 20:42:18 -0400

    added prompt questions to the const questions array

--- index.js
+++ index.js
@@ -5,7 +5,54 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your project title:',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description:',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter contribution guidelines:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
